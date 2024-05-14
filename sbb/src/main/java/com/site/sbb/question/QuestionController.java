d34883f2766d25153daeb08d3e46827578fce5e6
kimgyeomson <kgs960209@naver.com>
2024-05-14 17:49:14 +0900

    0514실습

--- sbb/src/main/java/com/site/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/site/sbb/question/QuestionController.java
@@ -1,23 +1,54 @@
 package com.site.sbb.question;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RequestMapping("/question")
 @Controller
 @RequiredArgsConstructor
 public class QuestionController {
 
-    private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
 
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public String list(Model model) {
-        List<Question> questionList = this.questionRepository.findAll();
+        List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
     }
+
+    @GetMapping(value = "/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
+    }
+
+    @GetMapping("/create")
+    public String questionCreate(QuestionForm questionForm) {
+        return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) { // 오류가 있는 경우 다시 제목과 내용을 작성하는 화면으로 돌아감
+            return "question_form";
+        }
+        // 오류가 없을 경우에만 질문이 등록되도록 만들어짐
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
+        return "redirect:/question/list";
+    }
+
+//    @PostMapping("/create")
+//    public String questionCreate(@RequestParam(value="subject") String subject,
+//                                 @RequestParam(value="content") String content) {
+//        this.questionService.create(subject, content);
+//        return "redirect:/question/list";
+//    }
 }
